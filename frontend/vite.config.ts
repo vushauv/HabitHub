c440e5a3bfade5154068fd8775b4cfebe9b08460
Vasili Vushau <vushauv@gmail.com>
2026-03-22 23:15:26 +0100

    fix: reconfigure vitetests so they are not loaded to the container

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-  },
   server: {
     host: "0.0.0.0",
     port: 3000,

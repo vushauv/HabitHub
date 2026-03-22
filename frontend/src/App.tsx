10d4d3662123561681d2f69340438c7c1279694b
Vasili Vushau <vushauv@gmail.com>
2026-03-22 22:51:48 +0100

    Ci/backend setup (#3)
    * feat: create asp app based on the webapi template
    
    * build: add EF Core and Npgsql packages
    
    * chore: add .env to gitignore
    
    * chore: add dockerignore
    
    * chore: add .idea to dockerignore
    
    * chore: add .env example
    
    * ci: add docker compose, it starts backend and postgres
    
    * ci: add docker compose overload (live server for developing)
    
    * chore: add *.log to gitignore
    
    * ci: add Dockerfile
    
    * chore: rm randomly generated files
    
    * chore: rm Properties dir (not used)
    
    * feat: add and get user endpoints, get settings, use migrations
    
    * chore: add .vscode to gitignore
    
    * docs: add docs for starting backend
    
    * refactor: change order of envs
    
    * test: add test logic
    
    * chore: update gitignore
    
    * docs: add repo url
    
    * docs: make so that dev branch is copied
    
    * fix: wait for postgres readiness before starting backend
    
    Add healthcheck to postgres service and use service_healthy condition
    so the backend doesn't attempt migrations before the database is ready.
    
    
    
    * feat: add frontend skeleton, update developers setup guide
    
    ---------

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}

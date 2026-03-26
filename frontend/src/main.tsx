451b3c5631791f41bccf0802cfd5211cde740f8d
Kamila Świercz <kamilaswiercz7@gmail.com>
2026-03-26 02:08:27 +0100

    HABITHUB-6 implement login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './home.css'
 import './register.css'
+import './login.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(

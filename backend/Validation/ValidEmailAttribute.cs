f96ed84e4e91da205266a6a28bf281356fe65396
Paula Matuszewska <116973696+paulamat1@users.noreply.github.com>
2026-04-08 23:42:15 +0200

    HABITHUB-19 added validation to login + register (#19)

---
+++ backend/Validation/ValidEmailAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace backend.Validation
+{
+    public class ValidEmailAttribute: ValidationAttribute
+    {
+        private static Regex EmailRegex = new(@"^[^\s@]+@[^\s@]+\.[^\s@]+$", RegexOptions.Compiled);
+
+        public ValidEmailAttribute()
+        {
+            ErrorMessage = "Invalid email format.";
+        }
+        public override bool IsValid(object? value)
+        {
+            string? email = value as string;
+            if (email == null)
+                return false;
+            return EmailRegex.IsMatch(email.Trim());
+        }
+    }
+}

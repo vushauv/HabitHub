83a9f7f725397b5a85378486275940bcd2c95149
paula <paulamatuszewska10@gmail.com>
2026-04-24 15:25:21 +0200

    HABITHUB-74 fix of creator has or does not have a membership issue

--- backend/Repositories/ITeamMemberRepository.cs
+++ backend/Repositories/ITeamMemberRepository.cs
@@ -8,7 +8,6 @@ public interface ITeamMemberRepository
     Task<TeamMember> CreateMemberAsync(TeamMember member);
     Task<TeamMember?> GetMemberByIdAsync(Guid memberId);
     Task<List<TeamMember>> GetMembersByIdsAsync(List<Guid> memberIds);
-
     Task UpdatePasswordAsync(Guid memberId, string newPasswordHash);
     Task ChangeEmailAsync(Guid memberId, string newEmail);
     Task<bool> EmailAlreadyExistsAsync(string email);

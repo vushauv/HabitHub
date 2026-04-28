4521539596d663cc6b5acb77f86d7f73a7fd9302
Sylwia <sylwia.rybak.72@gmail.com>
2026-04-29 00:12:01 +0200

    HABITHUB-55-backend-models-and-enums

---
+++ backend/Enums/EntryStatus.cs
@@ -0,0 +1,8 @@
+namespace backend.Enums;
+
+public enum EntryStatus
+{
+    Logged,
+    Pending,
+    Skipped
+}
--- backend/Models/HabitTeam.cs
+++ backend/Models/HabitTeam.cs
@@ -2,10 +2,11 @@ namespace backend.Models;
 
 public class HabitTeam
 {
-    public Guid TeamId {get; set;}
-    public string Name {get; set;} = string.Empty;
+    public Guid TeamId { get; set; }
+    public string Name { get; set; } = string.Empty;
     public TeamCreator Creator { get; set; } = null!;
-    public Guid CreatorId {get; set;}
-    public List<Membership> Memberships {get; set;} = new List<Membership>();
-    public List<InviteCode> InviteCodes {get;set;} = new List<InviteCode>();
+    public Guid CreatorId { get; set; }
+    public List<Membership> Memberships { get; set; } = new List<Membership>();
+    public List<InviteCode> InviteCodes { get;set; } = new List<InviteCode>();
+    public List<Habit> Habits { get; set; } = new List<Habit>();
 }

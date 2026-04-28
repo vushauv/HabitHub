2989f927e54d30c55b484ad3a1ba066ca72d9243
Sylwia Rybak <sylwwia.72@gmail.com>
2026-04-29 00:12:01 +0200

    HABITHUB-55-backend-models-and-enums

---
+++ backend/Models/HabitEntry.cs
@@ -0,0 +1,15 @@
+using backend.Enums;
+namespace backend.Models;
+
+public class HabitEntry
+{
+    public Guid HabitId { get; set; }
+    public Habit Habit { get; set; } = null!;
+    public Guid MemberId { get; set; }
+    public TeamMember Member { get; set; } = null!;
+    public Guid EntryId { get; set; }
+    public DateTime Date { get; set; }
+    public EntryStatus Status { get; set; }
+    public float? Value { get; set; }
+    public string? Notes { get; set; } = string.Empty;
+}

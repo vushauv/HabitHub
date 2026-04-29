77ada1cb6fad9a2bdeabb71816edbc4e556e3b13
Sylwia <sylwia.rybak.72@gmail.com>
2026-04-29 15:07:35 +0200

    HABITHUB-55-preapred-dtos-and-repository-interface

---
+++ backend/Dtos/HabitDtos/HabitResponseDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using backend.Enums;
+
+namespace backend.Dtos.AuthDtos
+{
+    public record HabitResponseDto(
+        Guid HabitId,
+        Guid TeamId,
+        string Name,
+        string? Goal,
+        Guid CreatorId,
+        HabitState HabitState,
+        HabitType HabitType,
+        Unit? Unit,
+        DateTime? ExpiryDate
+    );
+}
---
+++ backend/Dtos/HabitDtos/UpdateHabitRequestDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using backend.Enums;
+
+namespace backend.Dtos.AuthDtos
+{
+    public record UpdateHabitRequestDto(
+        [StringLength(256)]
+        string? Name,
+        [StringLength(512)]
+        string? Goal,
+        HabitType? HabitType,
+        Unit? Unit,
+        DateTime? ExpiryDate
+    );
+}

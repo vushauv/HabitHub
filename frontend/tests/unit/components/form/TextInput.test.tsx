91a4a4b720b2ed48f81dc2180429b6801de57eeb
Fumple <me@fumple.pl>
2026-05-13 10:35:14 +0200

    HABITHUB-83: Added TextInput tests

---
+++ frontend/tests/unit/components/form/TextInput.test.tsx
@@ -0,0 +1,93 @@
+import { fireEvent, render, renderHook, screen, waitFor } from "@testing-library/react";
+import { expect, it, vi } from "vitest";
+import TextInput from "../../../../src/components/form/TextInput";
+import { useForm } from "react-hook-form";
+import { useLens } from "@hookform/lenses";
+
+it("renders label, value and placeholder correctly", () => {
+  const { result } = renderHook(() => useForm({
+    defaultValues: { textInputTest: "John" }
+  }));
+  const { result: resultLens } = renderHook(() => useLens(result.current));
+
+  render(
+    <TextInput
+      label="TextInputTest"
+      lens={resultLens.current.focus("textInputTest")}
+      placeholder="hi :3"
+    />
+  );
+
+  const element = screen.getByLabelText("TextInputTest");
+  expect(element).toBeInTheDocument();
+  expect(element).toHaveValue("John");
+  expect(element).toHaveAttribute("placeholder", "hi :3");
+});
+
+it("handles value changes correctly", async () => {
+  const { result } = renderHook(() => useForm({
+    defaultValues: { textInputTest: "John" }
+  }));
+  const { result: resultLens } = renderHook(() => useLens(result.current));
+
+  const onSubmit = vi.fn();
+  render(
+    <form onSubmit={result.current.handleSubmit(onSubmit)}>
+      <TextInput
+        label="TextInputTest"
+        lens={resultLens.current.focus("textInputTest")}
+      />
+    </form>
+  );
+
+  const element = screen.getByLabelText("TextInputTest");
+  expect(element).toBeInTheDocument();
+
+  fireEvent.change(element, { target: { value: "John ;)" } });
+
+  expect(element).toHaveValue("John ;)");
+
+  fireEvent.submit(element);
+
+  await waitFor(() => {
+    expect(onSubmit).toHaveBeenCalledOnce();
+    expect(onSubmit.mock.lastCall![0]).toEqual({ textInputTest: "John ;)" });
+  });
+});
+
+it("is disabled when form is disabled", () => {
+  const { result } = renderHook(() => useForm({
+    defaultValues: { textInputTest: "John" },
+    disabled: true
+  }));
+  const { result: resultLens } = renderHook(() => useLens(result.current));
+
+  render(
+    <TextInput
+      label="TextInputTest"
+      lens={resultLens.current.focus("textInputTest")}
+    />
+  );
+
+  expect(screen.getByLabelText("TextInputTest")).toBeDisabled();
+});
+
+it("submits form correctly when submitted", () => {
+  const { result } = renderHook(() => useForm({
+    defaultValues: { textInputTest: "John" }
+  }));
+  const { result: resultLens } = renderHook(() => useLens(result.current));
+  
+  const onSubmit = vi.fn();
+
+  render(
+    <form onSubmit={onSubmit}>
+      <TextInput
+        label="TextInputTest"
+        lens={resultLens.current.focus("textInputTest")}
+      />
+    </form>);
+
+  fireEvent.submit(screen.getByLabelText("TextInputTest"));
+  expect(onSubmit).toHaveBeenCalledOnce();
+});
